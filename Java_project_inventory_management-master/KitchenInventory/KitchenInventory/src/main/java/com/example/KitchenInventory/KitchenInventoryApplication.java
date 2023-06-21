4744935ff8126b4853b94f52cfbf9ff024aecb2f
KulunuVidanage123 <123730095+KulunuVidanage123@users.noreply.github.com>
2023-06-21 13:58:50 +0530

    add kulunu

---
+++ Java_project_inventory_management-master/KitchenInventory/KitchenInventory/src/main/java/com/example/KitchenInventory/KitchenInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.KitchenInventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchenInventoryApplication.class, args);
+	}
+
+}
